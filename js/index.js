29a71db5b507d1da99935eb4f1c678aafef284c2
Silvio Póvoas <sjrpovoas@gmail.com>
2025-09-23 00:28:55 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,23 +1,2 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var navLinks = document.getElementById('nav-links');
-    var menuIcon = document.getElementById('menu-icon');
-    var menuLogo = document.getElementById('menu-logo');
-    // Visibilidade menu
-    navLinks.classList.toggle('active');
-
-    if (navLinks.classList.contains('active')) {
-        menuIcon.src = "./img/fechar-menu.png";
-        menuLogo.classList.add('visible');
-
-        // mudando os textos instantaneamente
-        item.addEventListener("click", () => {
-            navLinks.classList.toggle("active", false)
-            menuIcon.src = "./img/abrir-menu.png";
-            menuLogo.classList.remove('visible');
-        })
-
-    } else {
-        menuIcon.src = "/img/abrir-menu.png";
-        menuLogo.classList.remove('visible');
-    }
-});
+document.getElementById('menu-icon').addEventListener('click',function(){var navLinks=document.getElementById('nav-links');var menuIcon=document.getElementById('menu-icon');var menuLogo=document.getElementById('menu-logo');navLinks.classList.toggle('active');if(navLinks.classList.contains('active')){menuIcon.src="./img/fechar-menu.png";menuLogo.classList.add('visible');item.addEventListener("click",()=>{navLinks.classList.toggle("active",!1)
+menuIcon.src="./img/abrir-menu.png";menuLogo.classList.remove('visible')})}else{menuIcon.src="/img/abrir-menu.png";menuLogo.classList.remove('visible')}})
